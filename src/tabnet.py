17a94005374b4f5a1974c67a109544742ce1d7e2
AustinMadert <austin.madert@kiewit.com>
2021-05-21 12:11:06 -0500

    add decoder and encoder classes

--- src/tabnet.py
+++ src/tabnet.py
@@ -2,7 +2,35 @@ import torch
 import torch.nn as nn
 
 
+class TabNetEncoder(nn.Module):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+
+    def forward(self, X: torch.Tensor) -> torch.Tensor:
+        return X
+
+
+class TabNetDecoder(nn.Module):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+
+    def forward(self, X: torch.Tensor) -> torch.Tensor:
+        return X
+
+
 class TabNet(nn.Module):
 
     def __init__(self) -> None:
         super().__init__()
+        self.encoder = TabNetEncoder()
+        self.decoder = TabNetDecoder()
+
+
+    def forward(self, X: torch.Tensor) -> torch.Tensor:
+        X = self.encoder(X)
+        X = self.decoder(X)
+        return X
