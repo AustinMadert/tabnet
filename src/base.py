b8051fa9d5b8cc8b0376eaa1094cf974540c3a33
AustinMadert <austin.madert@kiewit.com>
2021-05-21 10:22:26 -0500

    Add init; Add src.base.py and class stubs

---
+++ src/base.py
@@ -0,0 +1,24 @@
+from typing import TypeVar
+
+import torch
+import torch.nn as nn
+
+ModelBase = TypeVar('ModelBase')
+
+
+class TorchBase(object):
+
+    def __init__(self) -> None:
+        pass
+
+
+    def build_dataset(self):
+        return NotImplementedError
+
+
+    def build_graph(self):
+        return NotImplementedError
+
+
+    def fit(self, X: torch.Tensor, y: torch.Tensor = None) -> ModelBase:
+        return self
