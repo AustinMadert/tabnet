c3092c39d071dbbf0d7d125e01bf28a7e49eafcf
AustinMadert <austin.madert@kiewit.com>
2021-05-22 12:02:01 -0500

    add basic structure and prior scales stub

--- src/attentive_transformer.py
+++ src/attentive_transformer.py
@@ -1,12 +1,21 @@
 import torch
 import torch.nn as nn
+from sparsemax import Sparsemax
 
 
 class AttentiveTransformer(nn.Module):
 
-    def __init__(self) -> None:
+    def __init__(self, input_size: int) -> None:
         super().__init__()
+        self.h = nn.linear(input_size, input_size)
+        self.bn = nn.BatchNorm2d()
+        self.sparse_max = Sparsemax(dim=-1)
+        self.prior_scales = torch.ones(input_size, input_size)
 
 
     def forward(self, X: torch.Tensor) -> torch.Tensor:
-        return X
+        X = self.h(X)
+        X = self.bn(X)
+        
+        out = self.sparse_max(X)
+        return out
