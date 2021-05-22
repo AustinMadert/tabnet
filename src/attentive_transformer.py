331a6575293cdb09afee98794dc74ebb480301a4
AustinMadert <austin.madert@kiewit.com>
2021-05-22 12:54:44 -0500

    add prior scales update

--- src/attentive_transformer.py
+++ src/attentive_transformer.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 import torch
 import torch.nn as nn
 from sparsemax import Sparsemax
@@ -5,17 +6,24 @@ from sparsemax import Sparsemax
 
 class AttentiveTransformer(nn.Module):
 
-    def __init__(self, input_size: int) -> None:
+    def __init__(self, input_size: int, rho: float = 1.0) -> None:
         super().__init__()
         self.h = nn.linear(input_size, input_size)
         self.bn = nn.BatchNorm2d()
-        self.sparse_max = Sparsemax(dim=-1)
-        self.prior_scales = torch.ones(input_size, input_size)
+        self.sparsemax = Sparsemax(dim=-1)
+        self.rho = rho
 
 
-    def forward(self, X: torch.Tensor) -> torch.Tensor:
-        X = self.h(X)
-        X = self.bn(X)
-        
-        out = self.sparse_max(X)
-        return out
+    def update_p(self, p: torch.Tensor, mask: torch.Tensor
+                            ) -> Tuple[torch.Tensor, torch.Tensor]:
+        update = self.rho - mask
+        p *= update
+        return p
+
+
+    def forward(self, a: torch.Tensor, p: torch.Tensor) -> torch.Tensor:
+        a = self.bn(self.h(a))
+        a = p * a
+        mask = self.sparsemax(a)
+        p = self.update_p(p)
+        return mask, p
