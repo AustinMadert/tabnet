588c180283ca14dc2fe074d5f9d3a292486c7f49
AustinMadert <austin.madert@kiewit.com>
2021-05-23 10:58:14 -0500

    Add forward to tabnetstep

--- src/encoder.py
+++ src/encoder.py
@@ -6,24 +6,29 @@ from transformers import FeatureBlock, AttentiveTransformer
 
 class TabNetStep(nn.Module):
 
-    def __init__(self, input_size: int) -> None:
+    def __init__(self, input_size: int, shared_feat_transformer: FeatureBlock) -> None:
         super().__init__()
         self.att = AttentiveTransformer(input_size=input_size)
+        self.shared_feat = shared_feat_transformer
         self.feat = self.build_feature_transformer()
-        self.p = self.build_p()
-
-
-    def build_p(self) -> torch.Tensor:
-        pass
+        self.p = torch.ones(self.input_size, self.input_size)
 
 
     def build_feature_transformer(self) -> nn.Module:
-        pass
+        return nn.Sequential(
+            self.shared_feat,
+            FeatureBlock(self.input_size)
+        )
 
 
     def forward(self, X: torch.Tensor, a: torch.Tensor) -> torch.Tensor:
-        a = self.att(a)
-        return X
+        mask, self.p = self.att(a, self.p)
+        X *= mask
+        X = self.feat(X)
+        # TODO: figure out current split operation
+        out, a = torch.split(X)
+        out = nn.ReLU(out)
+        return out, a, mask
 
 
 
