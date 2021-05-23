00bdeacb42c11a02645e719208dc5a5314373d91
AustinMadert <austin.madert@kiewit.com>
2021-05-22 21:24:56 -0500

    Add step module and method stubs

--- src/encoder.py
+++ src/encoder.py
@@ -1,17 +1,30 @@
 import torch
 import torch.nn as nn
 
-from feature_transformer import FeatureTransformer
+from transformers import FeatureBlock, AttentiveTransformer
 
 
-class AttentiveTransformer(nn.Module):
+class TabNetStep(nn.Module):
 
-    def __init__(self) -> None:
+    def __init__(self, input_size: int) -> None:
         super().__init__()
+        self.att = AttentiveTransformer(input_size=input_size)
+        self.feat = self.build_feature_transformer()
+        self.p = self.build_p()
 
 
-    def forward(self, X: torch.Tensor) -> torch.Tensor:
+    def build_p(self) -> torch.Tensor:
+        pass
+
+
+    def build_feature_transformer(self) -> nn.Module:
+        pass
+
+
+    def forward(self, X: torch.Tensor, a: torch.Tensor) -> torch.Tensor:
+        a = self.att(a)
         return X
+
 
 
 class TabNetEncoder(nn.Module):
