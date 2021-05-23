d66d2c7ceb8764f80fa56f73d6fcb39645544bb1
AustinMadert <austin.madert@kiewit.com>
2021-05-23 13:41:30 -0500

    Add init, builds, and forward to encoder

--- src/encoder.py
+++ src/encoder.py
@@ -1,10 +1,11 @@
+from typing import List
 import torch
 import torch.nn as nn
 
 from transformers import FeatureBlock, AttentiveTransformer
 
 
-class TabNetStep(nn.Module):
+class TabNetEncoderStep(nn.Module):
 
     def __init__(self, input_size: int, shared_feat_transformer: FeatureBlock) -> None:
         super().__init__()
@@ -25,8 +26,8 @@ class TabNetStep(nn.Module):
         mask, self.p = self.att(a, self.p)
         X *= mask
         X = self.feat(X)
-        # TODO: figure out current split operation
-        out, a = torch.split(X)
+        # Split activations from outputs in the columns dimension
+        out, a = X.split(X.shape[1] / 2, dim=1)
         out = nn.ReLU(out)
         return out, a, mask
 
@@ -34,9 +35,45 @@ class TabNetStep(nn.Module):
 
 class TabNetEncoder(nn.Module):
 
-    def __init__(self) -> None:
+    def __init__(self, input_size: int, batch_size: int,  n_steps: int = 10, 
+                output_size: int = None) -> None:
         super().__init__()
+        self.n_steps = n_steps
+        self.batch_size = batch_size
+        self.input_size = input_size * 2
+        if not output_size:
+            self.output_size = input_size
+        self.shared_feat = FeatureBlock(input_size=input_size, output_size=self.input_size)
+        self.feat = self.build_feature_transformer()
+        self.bn = nn.BatchNorm2d()
+        self.fc = nn.Linear(input_size, input_size)
+        self.steps = self.build_encoder_steps()
+        
+
+    def build_feature_transformer(self) -> nn.Module:
+        return nn.Sequential(
+            self.shared_feat,
+            FeatureBlock(self.input_size)
+        )
+
+    def build_encoder_steps(self) -> List[nn.Module]:
+        return [TabNetEncoderStep(self.input_size, self.shared_feat) 
+                for _ in range(self.n_steps)]
 
 
     def forward(self, X: torch.Tensor) -> torch.Tensor:
-        return X
+        X = self.bn(X)
+        X = self.feat(X)
+        _, a = X.split(X.shape[1] / 2, dim=1)
+        
+        attributes = torch.zeros(self.batch_size, self.input_size)
+        outputs = torch.zeros(self.batch_size, self.input_size)
+
+        for step in self.steps:
+            out, a, mask = step(X, a)
+            outputs += out
+            attributes += (mask * out)
+
+        outputs = self.fc(outputs)
+
+        return outputs, attributes
