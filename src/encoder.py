Fixed normalization spelling error

src/encoder.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 from torch.nn.modules.container import Sequential
 
 from transformers import FeatureBlock, AttentiveTransformer
-from normailzation import GhostBatchNormalization
+from normalization import GhostBatchNormalization
 
 
 class TabNetEncoderStep(nn.Module):
@@ -34,12 +34,20 @@ class TabNetEncoder(nn.Module):
     def __init__(self, shared_feat: nn.Sequential, n_d: int, batch_dim: int,  
                 output_dim: int, hidden_dim: int, n_steps: int) -> None:
         super().__init__()
+        self.n_steps = n_steps
+        self.batch_dim = batch_dim
         self.bn = GhostBatchNormalization(n_d, 8)
         self.fc = nn.Linear(n_d, output_dim)
         self.steps = [TabNetEncoderStep(shared_feat, n_d, batch_dim, hidden_dim) 
                       for _ in range(n_steps)]
         self.attributes = torch.zeros(batch_dim, n_d)
         self.output = torch.zeros(batch_dim, n_d)
+        self.reg = torch.Tensor([0.])
+
+
+    def sparsity_regularization(self, mask: torch.Tensor, eta: float = 0.00001) -> float:
+        reg = (-mask * torch.log(mask + eta)) / (self.n_steps * self.batch_dim)
+        return torch.sum(reg)
 
 
     def forward(self, X: torch.Tensor, a: torch.Tensor) -> torch.Tensor:
@@ -47,7 +55,8 @@ class TabNetEncoder(nn.Module):
             out, a, mask = step(X, a)
             self.output += out
             self.attributes += (mask * out)
+            self.reg += self.sparsity_regularization(mask)
 
         self.output = self.fc(self.output)
 
-        return self.output, self.attributes
+        return self.output, self.attributes, self.reg

src/model.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.utils.data
-from sklearn.
+from sklearn.metrics import mean_squared_error
 
 from model_bases import TorchModelBase
 from tabnet import TabNet
@@ -9,8 +9,10 @@ from tabnet import TabNet
 
 class TabNetRegressor(TorchModelBase):
 
-    def __init__(self, **kwargs) -> None:
+    def __init__(self, n_steps: int = 3, **kwargs) -> None:
         super().__init__(**kwargs)
+        self.n_steps = n_steps
+        self.loss = nn.MSELoss() if not 'loss' in kwargs else kwargs['loss']
 
     def build_dataset(self, X, y=None):
         self.n_d = X.shape[1]
@@ -19,9 +21,25 @@ class TabNetRegressor(TorchModelBase):
         return torch.utils.data.TensorDataset(X)
 
     def build_graph(self):
-        return TabNet(batch_dim=self.batch_size, n_d=self.n_d)
+        return TabNet(batch_dim=self.batch_size, n_d=self.n_d, n_steps=self.n_steps)
+
+    def forward_propagate(self, X_batch: torch.Tensor, y_batch: torch.Tensor):
+        batch_preds, self.attributes, reg = self.model(*X_batch)
+        err = self.loss(batch_preds, y_batch)
+        err += reg
+        return err
     
     def score(self, X, y, device=None):
         preds = self.predict(X, device=device)
-        return 
+        return mean_squared_error(y.to_numpy(), preds.to_numpy())
 
+
+
+from sklearn.datasets import load_boston
+X, y = load_boston(return_X_y=True)
+
+tn = TabNetRegressor(batch_size=16)
+tn.fit(X, y)
+print('Score: ', tn.score(X, y))
+
+print('Attributes: ', tn.attributes)

src/tabnet.py
@@ -4,7 +4,7 @@ from decoder import TabNetDecoder
 from encoder import TabNetEncoder
 from transformers import FeatureBlock
 from activations import GLU
-from normailzation import GhostBatchNormalization
+from normalization import GhostBatchNormalization
 
 
 class TabNet(nn.Module):
@@ -13,6 +13,8 @@ class TabNet(nn.Module):
                  output_dim: int = None, n_a: int = None) -> None:
         super().__init__()
         n_a = n_d if not n_a else n_a
+        if not output_dim:
+            output_dim = n_d
         hidden_dim = n_d + n_a
         self.splits = (n_d, n_a)
         self.shared_feat = nn.Sequential(
@@ -44,22 +46,6 @@ class TabNet(nn.Module):
         X = self.bn(X)
         out = self.shared_feat(X)
         _, a = out.split(self.splits, dim=1)
-        encoded, attributes = self.encoder(X, a)
+        encoded, attributes, reg = self.encoder(X, a)
         decoded = self.decoder(encoded)
-        return decoded, attributes
-
-
-from sklearn.datasets import load_boston
-X, y = load_boston(return_X_y=True)
-
-tn = TabNet(
-    n_d=X.shape[1], 
-    batch_dim=16, 
-    n_steps=3,
-    output_dim=X.shape[1],
-    n_a=X.shape[1]
-)
-X = torch.Tensor(X[:16, :])
-X, attributes = tn.forward(X)
-
-print(X, attributes)
+        return decoded, attributes, reg

src/transformers.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 from sparsemax import Sparsemax
 from activations import GLU
-from normailzation import GhostBatchNormalization
+from normalization import GhostBatchNormalization
 
 
 class AttentiveTransformer(nn.Module):
@@ -53,7 +53,3 @@ class FeatureBlock(nn.Module):
             # Normalization which halves Var
             out *= self.norm
         return out
-
-
-def sparsity_regularization(masks: torch.Tensor, eta: float = 0.00001) -> float:
-    pass